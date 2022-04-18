098be58c26bab9f269e7c735c1cbb51680682aad
Esteban Munoz Facusse <esteban.230@hotmail.com>
2022-04-18 12:27:26 -0600

    Adding drag and drop

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ReactNode } from "react";
+import "./App.css";
+import * as components from "@fluentui/react-components";
+import * as unstable_components from "@fluentui/react-components/unstable";
+import { DndProvider, DragPreviewImage, useDrag, useDrop } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <div className="container">
+        <div className="leftPanel">
+          {Object.keys(components).map((c) => (
+            <DropItem>
+              <unstable_components.Label>{c}</unstable_components.Label>
+            </DropItem>
+          ))}
+          {/* {Object.keys(unstable_components).map((c) => (
+            <DropItem>
+              <Label>{c}</Label>
+            </DropItem>
+          ))} */}
+          {/* <components.Button appearance="primary">Hello</components.Button> */}
+        </div>
+        <div className="canvas">
+          <DropContainer />
+        </div>
+      </div>
+    </DndProvider>
   );
 }
+
+type DropContainerProps = {
+  children?: ReactNode;
+};
+
+const DropContainer: React.FC<DropContainerProps> = ({ children }) => {
+  const [{ canDrop }, drop] = useDrop(
+    () => ({
+      accept: "item",
+      collect: (monitor) => ({
+        canDrop: !!monitor.canDrop(),
+      }),
+    }),
+    []
+  );
+  return (
+    <div className="dropContainer" ref={drop}>
+      <div>{children}</div>
+    </div>
+  );
+};
+
+type DropItemProps = {
+  children?: ReactNode;
+};
+
+const DropItem: React.FC<DropItemProps> = ({ children }) => {
+  const [{ isDragging }, drag, preview] = useDrag(
+    () => ({
+      type: "item",
+      collect: (monitor) => ({
+        isDragging: !!monitor.isDragging(),
+      }),
+    }),
+    []
+  );
+
+  return <div ref={drag}>{children}</div>;
+};
 
 export default App;
